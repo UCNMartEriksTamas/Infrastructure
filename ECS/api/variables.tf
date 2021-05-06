916d33561af901859a41b92a1e3b40d4fe4eec29
MII <mnel.jnr@gmail.com>
2021-05-06 10:48:38 +0200

    Copied some infrastructure work from previous projects

---
+++ ECS/api/variables.tf
@@ -0,0 +1,10 @@
+variable "project_name" {}
+variable "ecs_cluster" {}
+variable "public_subnet" {}
+variable "task_execution_role" {}
+variable "http_security_group" {}
+
+variable "api_repository_name" {}
+variable "api_service_name" {}
+variable "api_task_name" {}
+
---
+++ variables.tf
@@ -0,0 +1,33 @@
+# ------------ GENERAL SETUP -------------
+variable "region" {
+  default = "eu-central-1"
+}
+variable "cluster_name" {
+  default = "UCNSem1Project"
+}
+variable "project_name" {
+  default = "UCNSem1Project"
+}
+
+
+# ----------------- API ------------------
+variable "api_repository_name" {
+  default = "ucnsem1project-api"
+}
+variable "api_service_name" {
+  default = "UCNSem1project-api"
+}
+variable "api_task_name" {
+  default = "UCNSem1project-api"
+}
+
+
+# --------------- DATABASE ---------------
+variable "mssql_username" {
+  default = "user"
+}
+
+variable "mssql_password" {
+  default = "8C^Dv9#Bf%#&Fq7$zi3c"
+}
+
